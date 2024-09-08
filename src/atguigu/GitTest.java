0b7f2d7bc03506ff9fe9c92545169047d4700bbf
Cuiruohan <2752348544@qq.com>
2024-09-08 19:09:37 +0800

    Initial commit

---
+++ src/atguigu/GitTest.java
@@ -0,0 +1,8 @@
+package atguigu;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World11");
+    }
+}
